3dc5f0561cd6123269d1aee8de37b71d157252c6
osm3rr <osm3rr@mail.com>
2025-01-25 18:45:37 -0400

    work in progress

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">  
   import GetLocation from './components/GetLocation.vue';
+  import WeatherReport from './components/WeatherReport.vue';
 </script>
 
 <template>
   <GetLocation />
+  <WeatherReport />
 </template>
 
 <style scoped>
